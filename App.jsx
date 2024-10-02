cdd69ec2c7c9257a308b8fd472a57a642c7ba233
haider659 <haderkhan659@gmail.com>
2024-10-02 15:25:12 +0500

    react native1

--- App.jsx
+++ App.jsx
@@ -1,27 +1,13 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import ProductList from './components/Second';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the App</Text>
-      {/* Add your components or content here */}
-    </View>
-  );
+<>
+<ProductList />
+</>
+  )
+  
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
 
 export default App;
